be4a6b34b65a919e5b26c9d0987ea30105ad2893
Brayden Copley <braydenc303@gmail.com>
2019-02-20 16:12:18 -0700

    Transitions working properly and changed styling on h2's & h3's.

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,14 +1,6 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.div`
-    width:100%;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 50px;
-`
 
 const Paragraph = styled.p`
     font-family: Arial;
@@ -18,26 +10,24 @@ const Paragraph = styled.p`
     padding: 1rem;
 `
 
-const Spacer = styled.div`
-    height: 50px;
-`
 
 
 class About extends Component {
 
     render () {
         return (
-            <Wrapper id="About">
-                <Spacer></Spacer>
-                <h2>About</h2>
-                <Paragraph>Capable full-stack developer who loves the problem-solving aspect of programming. 
-                    Aptitude for creating experiences that are pleasing to the eye and which naturally lead users to where they want to go. 
-                    Drawing on my background in service and hospitality helps me to understand my clients wants and needs while using my technical skills in
-                    HTML, CSS, JavaScript, React, and NodeJS gained from the DU Full-Stack bootcamp to provide efficient and aesthetic solutions. 
-                </Paragraph>
-                <Paragraph>Integrity and a strong work ethic have to led me to management and other trusted positions in my previous work, and I look forward to my 
-                    future in this field building new technologies to make life and business more enjoyable.</Paragraph>
-            </Wrapper>
+            <div id="About" className="panel">
+                <div className="content">
+                    <h2>About</h2>
+                    <Paragraph>Capable full-stack developer who loves the problem-solving aspect of programming. 
+                        Aptitude for creating experiences that are pleasing to the eye and which naturally lead users to where they want to go. 
+                        Drawing on my background in service and hospitality helps me to understand my clients wants and needs while using my technical skills in
+                        HTML, CSS, JavaScript, React, and NodeJS gained from the DU Full-Stack bootcamp to provide efficient and aesthetic solutions. 
+                    </Paragraph>
+                    <Paragraph>Integrity and a strong work ethic have led me to management and other trusted positions in my previous work, and I look forward to my 
+                        future in this field building new technologies to make life and business more enjoyable.</Paragraph>
+                </div>
+            </div>
         )
     }
 }
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,10 +3,9 @@ import styled from "styled-components";
 
 
 const Wrapper = styled.div`
-    height: 100vh;
     display: flex;
     flex-direction: row;
-    align-items: baseline;
+    align-items: top;
     justify-content: space-evenly;
     flex-wrap: wrap;
 `
@@ -18,24 +17,21 @@ const Image = styled.img`
     }
 `
 
-const Spacer = styled.div`
-    height: 50px;
-`
 
 class Contact extends Component {
 
     render() {
         return (
-            <>
-                <Spacer id="Contact"></Spacer>
-                <h2>Contact</h2>
-                <Spacer></Spacer>
-                <Wrapper>
-                    <a href="https://github.com/braydenc303" target="top"><Image src="./assets/img/GitHub-Mark-120px-plus.png" alt="github logo" /></a> 
-                    <a className="LI-simple-link" href='https://www.linkedin.com/in/brayden-copley-9bb0b35?trk=profile-badge'><Image src="./assets/img/linkedin.png" alt="LinkedIn Logo"></Image></a>
-                    <a href="mailto:braydenc303@gmail.com"><Image src="./assets/img/email-icon-2.jpg" alt="email logo"/></a>
-                </Wrapper>
-            </>
+            <div id="Contact" className="panel">
+                <div className="content">
+                    <h2>Contact</h2>
+                    <Wrapper>
+                        <a href="https://github.com/braydenc303" target="top"><Image src="./assets/img/GitHub-Mark-120px-plus.png" alt="github logo" /></a> 
+                        <a className="LI-simple-link" href='https://www.linkedin.com/in/brayden-copley-9bb0b35?trk=profile-badge'><Image src="./assets/img/linkedin.png" alt="LinkedIn Logo"></Image></a>
+                        <a href="mailto:braydenc303@gmail.com"><Image src="./assets/img/email-icon-2.jpg" alt="email logo"/></a>
+                    </Wrapper>
+                </div>
+            </div>
         );
     }
 }
